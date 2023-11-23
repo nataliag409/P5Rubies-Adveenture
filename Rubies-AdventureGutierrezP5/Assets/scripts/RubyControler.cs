4e425e6b499e308e770a92ae52db3613aecf7a02
nataliag409 <natalia212329@gmail.com>
2023-11-23 01:15:25 -0800

    Added damagable, Added enimies, animated ruby and bot, was working on cogbullet.
    I added my damagable, and my enemies (bot). Then I animated Ruby and my Bot. Then started to work on my cogbullet but got tired. need to fix my cogbullet by adding in the right layers (caracter & projectile).
    Video: World Interactions
    Time: 25:02 (but probably need to re watch the while thing)

--- Rubies-AdventureGutierrezP5/Assets/scripts/RubyControler.cs
+++ Rubies-AdventureGutierrezP5/Assets/scripts/RubyControler.cs
@@ -1,23 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 public class RubyControler : MonoBehaviour
 {
     public float speed = 3.0f;
+
     public int maxHealth = 5;
+
+    public GameObject projectilePrefab;
+
+    public float timeInvincible = 2.0f;
     public int health { get { return currentHealth; } }
     int currentHealth;
 
+    bool isInvincible;
+    float invincibleTimer;
 
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
+
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
     // Start is called before the first frame update
     void Start()
     {
        rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -26,6 +38,29 @@ public class RubyControler : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if(invincibleTimer < 0 )
+            {
+                isInvincible = false;
+            }
+        }
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            Launch();
+        }
      
     }
     private void FixedUpdate()
@@ -38,7 +73,28 @@ public class RubyControler : MonoBehaviour
     }
     public void ChangeHealth(int amount)
     {
+        if(amount < 0)
+        {
+            animator.SetTrigger("Hit");
+
+            if(isInvincible)
+            {
+                return;
+            }
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
+    private void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+
+        animator.SetTrigger("Launch");
     }
 }
